5d57164fddf7597b2656680655abff74140f0af6
Jean F <jean.freitas@iesb.edu.br>
2025-11-25 17:48:22 -0300

    arquivos js criados e readMe atualizado

--- projeto-backend/app.js
+++ projeto-backend/app.js
@@ -1,20 +1,36 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const produtosRouter = require('./routes/produtosRouter');
+const apidocsRouter = require('./routes/apidocsRouter');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-app.use(logger('dev'));
+const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Conectar ao MongoDB
+const url = `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PSSWD}@${process.env.MONGO_DB_HOST}/${process.env.MONGO_DB_NAME}`;
+
+mongoose
+  .connect(url)
+  .then(() => console.log("Conectado ao MongoDB"))
+  .catch((err) => console.log("Erro ao conectar com mongoDB", err.message));
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB conectado'))
+  .catch(err => {
+    console.error('Erro ao conectar MongoDB:', err.message);
+    process.exit(1);
+  });
+
+// Rotas
+app.use('/produtos', produtosRouter);
+app.use('/api-docs', apidocsRouter);
+
+// Tratamento de erros simples
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status || 500).json({ msg: err.message || 'Erro interno' });
+});
 
 module.exports = app;
